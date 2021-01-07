9d08c25b421e5e8add8b26212a41a6bfdfe445d3
Sandro Lovnički <lovnicki.sandro@gmail.com>
2021-01-07 01:52:30 +0100

    [refactor] Home into home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,49 +14,6 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  int _currentTabIndex = 0;
-  GlobalKey<ScaffoldState> _rootScaffold = GlobalKey<ScaffoldState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _rootScaffold,
-      appBar: AppBar(
-        leading: GestureDetector(
-          onTap: () => _rootScaffold.currentState.openDrawer(),
-          child: Icon(Icons.menu),
-        ),
-        title: Text('CoverMe'),
-        centerTitle: true,
-      ),
-      drawer: Drawer(),
-      body: _currentTabIndex == 0
-          ? Container(color: Colors.pink[200])
-          : Container(color: Colors.green[200]),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentTabIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.event),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt),
-            label: 'Feed',
-          )
-        ],
-        onTap: (index) => setState(() => _currentTabIndex = index),
-      ),
     );
   }
 }
